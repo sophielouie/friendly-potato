9cfb89eb1cd6624340e55d50c0aa102abf2e61c9
timseymour42 <73562010+timseymour42@users.noreply.github.com>
2022-03-05 00:16:30 -0500

    Adding set-up packages

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import reactDOM from "react-dom";
+
+const App = () => (
+    //Return is implicit here
+    <div>
+        <hl>Hello React</hl>
+    </div>
+)
+
+export default App;
